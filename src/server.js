16e07f8b29f60fdddb3926ed08b38bc386ec8402
ooooorobo <orobos654@gmail.com>
2022-05-07 15:48:52 +0900

    [feat] 메인으로 redirect 시키기

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => res.render("home"))
+app.get("/*", (req, res) => res.redirect("/"))
 
-app.listen(PORT)
-console.log(`http://localhost:${PORT} 에서 서버 실행 중`)
+const handleListen = () => console.log(`http://localhost:${PORT} 에서 서버 실행 중`)
+app.listen(PORT, handleListen)
