a41d0ceed458d70c03105d541a7af081688c3d8a
ooooorobo <orobos654@gmail.com>
2022-05-07 15:42:18 +0900

    [feat] pug 템플릿, 정적 리소스 추가

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ import express from "express"
 const PORT = 3000
 const app = express()
 
-console.log(`http://localhost:${PORT} 에서 서버 실행 중`)
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => res.render("home"))
 
 app.listen(PORT)
+console.log(`http://localhost:${PORT} 에서 서버 실행 중`)
