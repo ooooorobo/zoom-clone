7d6aea786178abd6d5d2ce2d99c6e444bcb8c547
ooooorobo <orobos654@gmail.com>
2022-05-07 18:38:43 +0900

    [feat] 소켓 커넥션 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,7 @@
-console.log("app.js is working")
+const SOCKET_ENDPOINT = `ws://${window.location.host}`
+
+/**
+ * socket -> 연결된 서버
+ * WebSocket 인스턴스를 생성하면 서버와의 WS 커넥션을 생성함 - socket 인스턴스를 통해 서버와 통신 가능
+ */
+const socket = new WebSocket(SOCKET_ENDPOINT)
--- src/server.js
+++ src/server.js
@@ -5,6 +5,12 @@ import http from "http"
 const PORT = 3000
 const app = express()
 
+// 같은 port에서 웹 소켓 서버와 HTTP 서버를 함께 작동시키기 위해 위해 server 객체 접근이 필요
+const server = http.createServer(app)
+// webSocket server를 http 서버 위에 생성
+const wss = new WebSocket.Server({ server })
+
+/** HTTP **/
 app.set("view engine", "pug")
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"))
@@ -12,12 +18,18 @@ app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => res.render("home"))
 app.get("/*", (req, res) => res.redirect("/"))
 
+/** WS **/
+
+/**
+ * socket 연결되었을 때 처리
+ * @param socket - 연결된 브라우저
+ */
+function handleConnection(socket) {
+    console.log(socket)
+}
+
+wss.on('connection', handleConnection)
+
 const handleListen = () => console.log(`http://localhost:${PORT} 에서 서버 실행 중`)
-
-// 같은 port에서 웹 소켓 서버와 HTTP 서버를 함께 작동시키기 위해 위해 server 객체 접근이 필요
-const server = http.createServer(app)
-// webSocket server를 http 서버 위에 생성
-const wss = new WebSocket.Server({ server })
-
 // 같은 PORT(:3000)에서 HTTP, WS 두 가지 프로토콜을 처리할 수 있도록 함
 server.listen(PORT, handleListen)
